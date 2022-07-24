7d813f0ac8d814653761d0d4d7cae7bfe657917e
MatheusTh1 <matheushenriquesilva.th@gmail.com>
2022-07-24 11:59:29 -0300

    inicializado o projeto com spring

---
+++ src/test/java/mergulho/spring/algaworks/AlgaworksApplicationTests.java
@@ -0,0 +1,13 @@
+package mergulho.spring.algaworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgaworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
